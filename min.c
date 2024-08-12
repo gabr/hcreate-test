d70906346f8da0d27fb9673a49058e8072099df1
gabr <arkadiuszgabrys@gmail.com>
2024-08-12 10:48:54 +0200

    init

---
+++ min.c
@@ -0,0 +1,41 @@
+#if 0
+#!/usr/bin/env sh
+set -e # stop on first error
+cc "$0" -o a.out && ./a.out
+exit
+#endif
+
+/*
+ * Does exactly the same thing as main.c but the code
+ * has been reduced to its minimalistic C form.
+ */
+
+#include <search.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static char *data[] = { "alpha", "bravo", "charlie", "delta",
+    "echo", "foxtrot", "golf", "hotel", "india", "juliet",
+    "kilo", "lima", "mike", "november", "oscar", "papa",
+    "quebec", "romeo", "sierra", "tango", "uniform",
+    "victor", "whisky", "x-ray", "yankee", "zulu"
+};
+
+#define LEN(a) (sizeof(a)/sizeof(*a))
+#define FORMAT "%9.9s -> %9.9s:%d\n"
+
+int main(void)
+{
+    size_t i;
+    ENTRY *e;
+    hcreate(30);
+    /* insert all but two last elements */
+    for (i=0;i<LEN(data)-2;i++) hsearch((ENTRY){data[i],(void*)i}, ENTER);
+    /* try to find last four elements - two should be missing */
+    for (i=LEN(data)-4;i<LEN(data);i++) {
+        if(e = hsearch((ENTRY){data[i]}, FIND))
+             printf(FORMAT, data[i], e->key, (size_t)(e->data));
+        else printf(FORMAT, data[i], "NULL", 0);
+    }
+    return 0;
+}
